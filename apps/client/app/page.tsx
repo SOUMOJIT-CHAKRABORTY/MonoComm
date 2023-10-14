8337929c271e0f05831a1bc02b005d58bbbb3100
SOUMOJIT-CHAKRABORTY <goutamghambir2018@gmail.com>
2023-10-14 22:48:07 +0530

    new ui color added to login form page

--- apps/client/app/page.tsx
+++ apps/client/app/page.tsx
@@ -2,9 +2,11 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <div>
-      <h1 className="text-2xl">Home</h1>
-      <button className="btn btn-primary">One</button>
+    <div className="flex flex-col justify-center items-center mx-auto h-screen bg-gradient-to-r from-green-300 to-purple-400">
+      <div className="artboard phone-1 gap-6 border rounded-lg bg-white  flex flex-col justify-center items-center">
+        <h1 className="text-2xl">Home</h1>
+        <button className="btn btn-primary">One</button>
+      </div>
     </div>
   );
 }
