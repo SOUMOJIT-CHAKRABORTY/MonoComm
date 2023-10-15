697d3e234e27ae53b1fdbafcb5cbbe8be85f674c
SOUMOJIT-CHAKRABORTY <goutamghambir2018@gmail.com>
2023-10-15 23:22:38 +0530

    navbar component added

--- apps/client/app/page.tsx
+++ apps/client/app/page.tsx
@@ -1,12 +1,16 @@
+import Navbar from "@/components/Navbar";
 import Image from "next/image";
 
 export default function Home() {
   return (
-    <div className="flex flex-col justify-center items-center mx-auto h-screen bg-gradient-to-r from-green-300 to-purple-400">
-      <div className="artboard phone-1 gap-6 border rounded-lg bg-white  flex flex-col justify-center items-center">
-        <h1 className="text-2xl">Home</h1>
-        <button className="btn btn-primary">One</button>
+    <section className="h-screen">
+      <Navbar />
+      <div className="flex flex-col justify-center h-screen items-center mx-auto bg-gradient-to-r from-green-300 to-purple-400">
+        <div className="artboard phone-1 gap-6 border rounded-lg bg-white  flex flex-col justify-center items-center">
+          <h1 className="text-2xl">Home</h1>
+          <button className="btn btn-primary">One</button>
+        </div>
       </div>
-    </div>
+    </section>
   );
 }
